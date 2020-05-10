6b951ced9681be9da7d51bd8fc48d6fd77b10a23
Nick Howell <nick@nickhowell.co.uk>
2020-05-10 11:54:44 +0100

    add some unit tests

--- spec/classes/foldingathome_spec.rb
+++ spec/classes/foldingathome_spec.rb
@@ -8,6 +8,17 @@ describe 'foldingathome' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it { is_expected.to contain_package('fahclient').with_ensure('installed') }
+      it { is_expected.to contain_File('/etc/fahclient').with(
+        ensure: 'directory',
+        )
+      }
+      it { is_expected.to contain_File('/etc/fahclient/config.xml').with(
+        ensure: 'file',
+        require: 'File[/etc/fahclient]',
+        notify: 'Service[FAHClient]',
+        )
+      }
     end
   end
 end
