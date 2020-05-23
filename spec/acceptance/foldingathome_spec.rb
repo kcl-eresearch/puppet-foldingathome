0ab6bd4deab9a443012517d2c82f97f8d28e698c
Nick Howell <nick@nickhowell.co.uk>
2020-05-23 11:23:45 +0100

    add an extra parameter so we don't have to start FAHClient for the tests

--- spec/acceptance/foldingathome_spec.rb
+++ spec/acceptance/foldingathome_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper_acceptance'
 
 pp_basic = <<-PUPPETCODE
   class {'foldingathome':
-    gpu_enable => false,
-    user_name => 'fahpuppettestuser',
-    user_passkey => '12345678901234567890123456789012',
-    allow => '0/0',
-    web_allow => '0/0',
-    team_id => 0,
-    cpu_slots => {
+    gpu_enable     => false,
+    user_name      => 'fahpuppettestuser',
+    user_passkey   => '12345678901234567890123456789012',
+    allow          => '0/0',
+    web_allow      => '0/0',
+    team_id        => 0,
+    cpu_slots      => {
         '0' => '1',
         '1' => '1',
     },
+    service_ensure => 'stopped'
   }
 PUPPETCODE
 
@@ -19,7 +20,7 @@ idempotent_apply(pp_basic)
 describe 'foldingathome' do
   describe service('FAHClient') do
     it { is_expected.to be_enabled }
-    it { is_expected.to be_running }
+    it { is_expected.not_to be_running }
   end
 
   describe file('/etc/fahclient/config.xml') do
